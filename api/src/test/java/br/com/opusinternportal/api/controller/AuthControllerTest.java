995a1516f200d4169e9bfc7855880400952f54c6
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-01-27 08:47:47 -0300

    Implements AuthController unit tests

--- api/src/test/java/br/com/opusinternportal/api/controller/AuthControllerTest.java
+++ api/src/test/java/br/com/opusinternportal/api/controller/AuthControllerTest.java
@@ -1,106 +1,202 @@
 package br.com.opusinternportal.api.controller;
 
+import br.com.opusinternportal.api.dto.GenericMessage;
+import br.com.opusinternportal.api.dto.RegisterRequest;
+import br.com.opusinternportal.api.entity.PortalUser;
+import br.com.opusinternportal.api.entity.Role;
+import br.com.opusinternportal.api.repository.PortalUserRepository;
 import br.com.opusinternportal.api.util.EnvironmentVariablesLoader;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
-@Transactional
-@Rollback
+@AutoConfigureJsonTesters
+@ExtendWith(MockitoExtension.class)
 class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private JacksonTester<RegisterRequest> registerRequestJson;
+
+    @Autowired
+    private JacksonTester<GenericMessage> genericMessageJson;
+
+    @Value("${opusinternportal.email-domain}")
+    String emailDomain;
 
     @BeforeAll
     static void setUp() throws Exception {
         EnvironmentVariablesLoader.loadEnvironmentVariables();
     }
 
+    @MockitoBean
+    private Optional<PortalUser> portalUserOptional;
+
+    @MockitoBean
+    private PortalUserRepository portalUserRepository;
+
     @Test
-    @DisplayName("Registration of an intern with correct email format and extension show return successful message")
+    @DisplayName("Registration of an intern with correct email format and extension should return successful message")
     void registerTestScenario1() throws Exception {
-        String registerRequestJson = """
-                {
-                    "email": "user@opus-software.com.br",
-                    "password": "password",
-                    "role": "INTERN"
-                }
-        """;
+        // Arrange
+        Mockito.when(portalUserOptional.isPresent()).thenReturn(false);
 
-        mockMvc.perform(post("/api/auth/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(registerRequestJson))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json("""
-                        {
-                            "message": "User registered successfully!"
-                        }
-                """));
+        var registerRequest = new RegisterRequest("user" + emailDomain, "password123", Role.INTERN);
+
+        // Act
+        var response = mockMvc
+                .perform(
+                        post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(registerRequestJson.write(registerRequest).getJson())
+                )
+                .andReturn().getResponse();
+
+
+        // Assert
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var expectedResponse = genericMessageJson.write(
+                new GenericMessage("User registered successfully!")
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(expectedResponse);
     }
 
-
     @Test
-    @DisplayName("Registration of mentor with correct email format and extension show return successful message")
+    @DisplayName("Registration of a mentor with correct email format and extension should return successful message")
     void registerTestScenario2() throws Exception {
-        String registerRequestJson = """
-                {
-                    "email": "user@opus-software.com.br",
-                    "password": "password",
-                    "role": "MENTOR"
-                }
-        """;
+        // Arrange
+        Mockito.when(portalUserOptional.isPresent()).thenReturn(false);
 
-        mockMvc.perform(post("/api/auth/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(registerRequestJson))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json("""
-                        {
-                            "message": "User registered successfully!"
-                        }
-                """));
+        var registerRequest = new RegisterRequest("user" + emailDomain, "password123", Role.MENTOR);
+
+        // Act
+        var response = mockMvc
+                .perform(
+                        post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(registerRequestJson.write(registerRequest).getJson())
+                )
+                .andReturn().getResponse();
+
+
+        // Assert
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var expectedResponse = genericMessageJson.write(
+                new GenericMessage("User registered successfully!")
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(expectedResponse);
+    }
+
+
+    @Test
+    @DisplayName("Registration of an intern with incorrect email extension should return error message")
+    void registerTestScenario3() throws Exception {
+        // Arrange
+        Mockito.when(portalUserOptional.isPresent()).thenReturn(false);
+
+        var registerRequest = new RegisterRequest("user" + emailDomain + ".other", "password123", Role.INTERN);
+
+        // Act
+        var response = mockMvc
+                .perform(
+                        post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(registerRequestJson.write(registerRequest).getJson())
+                )
+                .andReturn().getResponse();
+
+
+        // Assert
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        var expectedResponse = genericMessageJson.write(
+                new GenericMessage("Email must belong to the domain: " + emailDomain)
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(expectedResponse);
     }
 
     @Test
-    @DisplayName("Registration of an Administrator should not be allowed")
-    void registerTestScenario3() throws Exception {
-        String registerRequestJson = """
-                {
-                    "email": "user@opus-software.com.br",
-                    "password": "password",
-                    "role": "ADMIN"
-                }
-        """;
+    @DisplayName("Trying to register an administrator should return error message")
+    void registerTestScenario4() throws Exception {
+        // Arrange
+        Mockito.when(portalUserOptional.isPresent()).thenReturn(false);
 
-        mockMvc.perform(post("/api/auth/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(registerRequestJson))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json("""
-                        {
-                        	"message": "Cannot register as an administrator!"
-                        }
-                """));
+        var registerRequest = new RegisterRequest("user" + emailDomain, "password123", Role.ADMIN);
+
+        // Act
+        var response = mockMvc
+                .perform(
+                        post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(registerRequestJson.write(registerRequest).getJson())
+                )
+                .andReturn().getResponse();
+
+
+        // Assert
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        var expectedResponse = genericMessageJson.write(
+                new GenericMessage("Cannot register as an administrator!")
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(expectedResponse);
+    }
+
+
+    @Test
+    @DisplayName("Trying to register with registered email should return error message")
+    void registerTestScenario5() throws Exception {
+        // Arrange
+        Mockito.when(portalUserRepository.findByEmail(Mockito.any()))
+                .thenReturn(Optional.of(new PortalUser(null, "email", "pass", Role.INTERN)));
+
+        var registerRequest = new RegisterRequest("user" + emailDomain, "password123", Role.MENTOR);
+
+        // Act
+        var response = mockMvc
+                .perform(
+                        post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(registerRequestJson.write(registerRequest).getJson())
+                )
+                .andReturn().getResponse();
+
+
+        // Assert
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        var expectedResponse = genericMessageJson.write(
+                new GenericMessage("Email is already in use!")
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(expectedResponse);
     }
 }
